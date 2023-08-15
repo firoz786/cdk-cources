1020a735baf195a89d15e0a3e519b4877f5b80f6
firoz <firoz.alam@in.abb.com>
2023-08-15 15:14:48 +0530

    aws-sdk

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,11 +1,8 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { ITable } from "aws-cdk-lib/aws-dynamodb";
-import {
-  Code,
-  Function as LambdaFunction,
-  Runtime,
-} from "aws-cdk-lib/aws-lambda";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import {  Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
@@ -28,6 +25,15 @@ export class LambdaStack extends Stack {
         TABLE_NAME : props.spacesTable.tableName
       }
     });
+
+    helloLambda.addToRolePolicy(new PolicyStatement({
+      effect:Effect.ALLOW,
+      actions:[
+        's3:ListAllMyBuckets',
+        's3:ListBucket'
+      ],
+      resources:['*']
+    }))
 
     this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
--- src/services/hello.ts
+++ src/services/hello.ts
@@ -4,12 +4,23 @@ import {
   Context,
 } from "aws-lambda";
 import { v4 } from "uuid";
+import { S3Client ,ListBucketsCommand} from '@aws-sdk/client-s3';
+
+const s3Client = new S3Client({});
+
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+
+  const command = new ListBucketsCommand({});
+  const listBucketsCommand = (await (s3Client.send(command))).Buckets;
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
-    body: JSON.stringify("Hello from lambda, this is the uuid : "+v4()),
+    body: JSON.stringify("Hello from lambda, here are your buckets : "+
+    JSON.stringify(listBucketsCommand)),
   };
+
   console.log("event-firoz",event);
+
   return response;
 }
 
